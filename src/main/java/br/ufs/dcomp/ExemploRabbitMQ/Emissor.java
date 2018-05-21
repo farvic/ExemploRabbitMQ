3df2bab9d90b99941801d04eb987f9c338d73ac7
Victor Fonseca Araujo <victorfa@dcomp.ufs.br>
2018-05-21 16:17:43 +0000

    RabbitMQ

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
@@ -10,12 +10,21 @@ public class Emissor {
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri("amqp://...");
+    //factory.setUri("amqp://anvmucwf:AOFmi6z64Jv7USJidYyuNhFvzbI3o6WV@mosquito.rmq.cloudamqp.com/anvmucwf"); //Ariel
+    factory.setUri("amqp://psbewhvz:s9vIx8znYBvSvy3L0yewQZWlPSkoU_ss@fly.rmq.cloudamqp.com/psbewhvz"); //Neyara
+    //factory.setUri("amqp://kusswacx:J372cualNgTdoneGQmlab4hbc-J3c-Ty@fly.rmq.cloudamqp.com/kusswacx""); //Eu
+    
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    String message = "Olá!!!";
+    String message = "Vms tc?";
+    
+    /*Publicando em uma fila
+    * "": exchange não especificado
+    * QUEUE_NAME: fila
+    * message.getBytes(): mensagem como vetor de bytes
+    */
     channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
     System.out.println(" [x] Mensagem enviada: '" + message + "'");
 
--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Receptor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Receptor.java
@@ -9,17 +9,22 @@ public class Receptor {
   private final static String QUEUE_NAME = "minha-fila";
 
   public static void main(String[] argv) throws Exception {
+    //Estabelecendo conex'ao TCP com o servidor AMQP
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri("amqp://...");
+    factory.setUri("amqp://kusswacx:J372cualNgTdoneGQmlab4hbc-J3c-Ty@fly.rmq.cloudamqp.com/kusswacx");
     Connection connection = factory.newConnection();
+    //Canal de comunicação independente
     Channel channel = connection.createChannel();
-
+    
+    //Criar uma fila no servidor
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
     System.out.println(" [*] Esperando recebimento de mensagens...");
 
     Consumer consumer = new DefaultConsumer(channel) {
+      //Indica o que serã feito quando a mensagem chegar
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
           throws IOException {
+            //Mapeando a mensagem para uma string
         String message = new String(body, "UTF-8");
         System.out.println(" [x] Mensagem recebida: '" + message + "'");
       }
