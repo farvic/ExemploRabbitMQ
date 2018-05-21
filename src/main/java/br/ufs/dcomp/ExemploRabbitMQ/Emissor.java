d22218a55566293e4ff3e9661b39d004bdeb7561
Victor Fonseca Araujo <victorfa@dcomp.ufs.br>
2018-05-21 23:42:36 +0000

    Added input from user

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
@@ -4,6 +4,8 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.util.Scanner;
+
 public class Emissor {
 
   private final static String QUEUE_NAME = "minha-fila";
@@ -11,23 +13,35 @@ public class Emissor {
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
     //factory.setUri("amqp://anvmucwf:AOFmi6z64Jv7USJidYyuNhFvzbI3o6WV@mosquito.rmq.cloudamqp.com/anvmucwf"); //Ariel
-    factory.setUri("amqp://psbewhvz:s9vIx8znYBvSvy3L0yewQZWlPSkoU_ss@fly.rmq.cloudamqp.com/psbewhvz"); //Neyara
-    //factory.setUri("amqp://kusswacx:J372cualNgTdoneGQmlab4hbc-J3c-Ty@fly.rmq.cloudamqp.com/kusswacx""); //Eu
+    //factory.setUri("amqp://psbewhvz:s9vIx8znYBvSvy3L0yewQZWlPSkoU_ss@fly.rmq.cloudamqp.com/psbewhvz"); //Neyara
+    factory.setUri("amqp://kusswacx:J372cualNgTdoneGQmlab4hbc-J3c-Ty@fly.rmq.cloudamqp.com/kusswacx"); //Eu
     
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
+    
+    Scanner sc = new Scanner(System.in);
 
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    String message = "Vms tc?";
+    //String message = "Vms tc?";
     
-    /*Publicando em uma fila
-    * "": exchange não especificado
-    * QUEUE_NAME: fila
-    * message.getBytes(): mensagem como vetor de bytes
-    */
-    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-    System.out.println(" [x] Mensagem enviada: '" + message + "'");
-
+    int cnt = 0;
+    
+    while(cnt<3){
+      
+      String message = sc.nextLine();
+    
+      /*Publicando em uma fila
+      * "": exchange não especificado
+      * QUEUE_NAME: fila
+      * message.getBytes(): mensagem como vetor de bytes
+      */
+      channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+      System.out.println(" [x] Mensagem enviada: '" + message + "'");
+      
+      cnt++;
+      
+    }
+    
     channel.close();
     connection.close();
   }
